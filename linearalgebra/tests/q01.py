8ecd186cb6769a8b03317ddb0d9bca9bddb0dee8
Charles Frye <charlesfrye@berkeley.edu>
2020-05-11 22:11:11 -0700

    adds initial tests for dot product example

--- linearalgebra/tests/q01.py
+++ linearalgebra/tests/q01.py
@@ -1,23 +1,38 @@
 test = {
-    "name": "First Test of OK on Colab",
+    "name": "Dot Product Example",
     "points": 1,
     "suites": [
         {
             "cases": [
                 {
                     "code": r"""
-                    >>> ## autopass
-                    >>> True
+                    >>> ## all-zeros vector should always return 0
+                    >>> int(dot(all_zeros, all_zeros))
+                    0
+                    >>> int(dot(all_zeros, random_vector))
+                    0
+                    >>> ## dot product with all-ones vector is sum
+                    >>> ## sum of numbers up to 5 is 15
+                    >>> int(dot(range_vector, all_ones))
+                    15
+                    >>> ## sum needs to also work on a random vector
+                    >>> np.isclose(sum(random_vector), dot(random_vector, all_ones))
+                    >>> ## and we should get same answer as numpy in general
+                    >>> np.isclose(np.dot(random_vector, second_random_vector),
+                    >>>    dot(random_vector, second_random_vector))
                     True
-                    >>> ## autopass
-                    >>> print("hello")
-                    hello
                     """,
                     "hidden": False,
                     "locked": False
                 }
             ],
             "setup": r"""
+            shape = 5
+            range_vector = np.array([1, 2, 3, 4, 5])
+            all_zeros = np.zeros(shape)
+            all_ones = np.ones(shape)
+            random_vector = np.random.randn(shape)
+            second_random_vector = np.random.randn(shape)
             """,
             "teardown": r"""
             """,
