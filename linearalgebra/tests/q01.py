fc4c152b517d1c825c1745dc96ac94fcffc03b15
Charles Frye <charlesfrye@berkeley.edu>
2020-05-11 20:34:30 -0700

    testing ok on colab

---
+++ linearalgebra/tests/q01.py
@@ -0,0 +1,25 @@
+test = {
+    "name": "First Test of OK on Colab",
+    "points": 1,
+    "suites": [
+        {
+            "cases": [
+                {
+                    "code": r"""
+                    >>> ## autopass
+                    >>> True
+                    True
+                    >>> ## autopass
+                    >>> isinstance(conch_st_residents, list)
+                    True
+                    """,
+                    "hidden": False,
+                    "locked": False
+                }
+            ],
+            "setup": r"""
+            """,
+            "teardown": r"""
+            """,
+            "type": "doctest"}]
+        }
