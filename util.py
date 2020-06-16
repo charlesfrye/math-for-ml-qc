bf587b9d43edc157e53b805909ca77d09c58a385
Charles Frye <charlesfrye@berkeley.edu>
2020-06-16 11:16:51 -0700

    allows entity setting in WandbTrackedOK

--- util.py
+++ util.py
@@ -4,22 +4,22 @@ import wandb
 
 class WandbTrackedOK(object):
 
-  def __init__(self, path, project):
-    self.grader = Notebook(path)
-    wandb.init(entity="charlesfrye", project=project, anonymous="must")
-    self.test_map = self.grader.assignment.test_map
-    self.pass_dict = {k: 0 for k in self.test_map}
-    self.log()
+    def __init__(self, entity, path, project):
+        self.grader = Notebook(path)
+        wandb.init(entity=entity, project=project, anonymous="must")
+        self.test_map = self.grader.assignment.test_map
+        self.pass_dict = {k: 0 for k in self.test_map}
+        self.log()
 
-  def grade(self, question, *args, **kwargs):
-    result = self.grader.grade(question, *args, **kwargs)
-    self.pass_dict[question] = result["passed"]
-    self.log()
+    def grade(self, question, *args, **kwargs):
+        result = self.grader.grade(question, *args, **kwargs)
+        self.pass_dict[question] = result["passed"]
+        self.log()
 
-  def log(self):
-    total = sum([v for  v in self.pass_dict.values()])
-    wandb.log({"passes": self.pass_dict,
-               "total": total})
+    def log(self):
+        total = sum([v for v in self.pass_dict.values()])
+        wandb.log({"passes": self.pass_dict,
+                   "total": total})
 
-  def __delete__(self):
-    wandb.join()
+    def __delete__(self):
+        wandb.join()
