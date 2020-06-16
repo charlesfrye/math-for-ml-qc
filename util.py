8df7a711d1ffc4e5334894c8fe48e43d08ea4785
Charles Frye <charlesfrye@berkeley.edu>
2020-06-15 17:13:46 -0700

    adds WandbTrackedOK autograder

---
+++ util.py
@@ -0,0 +1,25 @@
+from client.api.notebook import Notebook
+import wandb
+
+
+class WandbTrackedOK(object):
+
+  def __init__(self, path, project):
+    self.grader = Notebook(path)
+    wandb.init(entity="charlesfrye", project=project, anonymous="must")
+    self.test_map = self.grader.assignment.test_map
+    self.pass_dict = {k: 0 for k in self.test_map}
+    self.log()
+
+  def grade(self, question, *args, **kwargs):
+    result = self.grader.grade(question, *args, **kwargs)
+    self.pass_dict[question] = result["passed"]
+    self.log()
+
+  def log(self):
+    total = sum([v for  v in self.pass_dict.values()])
+    wandb.log({"passes": self.pass_dict,
+               "total": total})
+
+  def __delete__(self):
+    wandb.join()
