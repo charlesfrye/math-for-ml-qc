62a6ce845275b9d2fec4f8307010bb554872ed67
Charles Frye <charlesfrye@berkeley.edu>
2020-06-22 16:09:07 -0700

    moves wandb folder inside lesson folder

--- math-for-ml-qc/util.py
+++ math-for-ml-qc/util.py
@@ -4,9 +4,9 @@ import wandb
 
 class WandbTrackedOK(object):
 
-    def __init__(self, entity, path, project):
-        self.grader = Notebook(path)
-        wandb.init(entity=entity, project=project, anonymous="must")
+    def __init__(self, entity, ok_path, project, wandb_path="./wandb"):
+        self.grader = Notebook(ok_path)
+        wandb.init(entity=entity, project=project, dir=wandb_path, anonymous="must")
         self.test_map = self.grader.assignment.test_map
         self.pass_dict = {k: 0 for k in self.test_map}
         self.log()
